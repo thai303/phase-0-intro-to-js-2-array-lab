ea0221bd72ed7b879d876bfb6bb4918dcc8f342e
Thai Xiong <thai303@hotmail.com>
2022-05-18 13:48:20 -0600

    commit tree 8316c477e719c73e1ade979bea55fe82685bce4f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const name = ['Milo', 'Otis', 'Garfield']
+const cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.push(name)
+    return copyOfCats
+}
+function prependCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.unshift(name)
+    return copyOfCats
+}
+function removeLastCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.pop(name)
+    return copyOfCats
+}
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.shift(name)
+    return copyOfCats
+}
